e10861994b49375403c82d9dfbe6be74f9a944d6
Tinchocw <mcwikla@fi.uba.ar>
2025-03-18 19:35:34 -0300

    add: validation of echo server

---
+++ validar-echo-server.sh
@@ -0,0 +1,19 @@
+
+ECHO_SERVER_CONTAINER="sever"
+MESSAGE="message-validation"
+NET="my-net"
+
+
+docker network create $NET  # Creo la red por la que me voy a comunicar 
+docker network connect $NET server # Conecto el server a la red
+
+RESULT=docker (run --rm -network $NET alpine /bin/sh -c "echo '$MENSAJE' | nc $CONTENEDOR_ECHO_SERVER 12345") # Corro el cliente
+
+if [ "$RESULT" == "$MENSAJE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
+
+python3 server-validation.py
+
