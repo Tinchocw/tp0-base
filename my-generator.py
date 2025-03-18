5ccc373fa77320414c3a0d1a9a13e8ceae370354
Tinchocw <mcwikla@fi.uba.ar>
2025-03-17 23:54:44 -0300

    refactor: Change structure of the docker-compose generator

--- my-generator.py
+++ my-generator.py
@@ -20,6 +20,7 @@ def add_clients(num_clients, data):
                 'CLI_LOG_LEVEL=DEBUG'
             ],
             'networks': ['testing_net'],
+            'volumes': ['/client/config.yaml:/config.yaml'],
             'depends_on': ['server']
         }
     
@@ -36,6 +37,7 @@ def generate_docker_compose(num_clients, output_file):
                     'PYTHONUNBUFFERED=1',
                     'LOGGING_LEVEL=DEBUG'
                 ],
+                'volumes': ['./server/config.ini:/config.ini'],
                 'networks': ['testing_net']
             }
         },
