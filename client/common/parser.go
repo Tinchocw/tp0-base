7e44f5a573f5a23740e6fceb96a56b0aa61f9809
Tinchocw <mcwikla@fi.uba.ar>
2025-03-23 12:09:25 -0300

    add: Paser of cvs

---
+++ client/common/parser.go
@@ -0,0 +1,72 @@
+package common
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Parser struct {
+	file      *os.File
+	maxBatch  int
+	bufReader *bufio.Reader
+	agency    int
+}
+
+func NewParser(filename string, maxBatch int, agency string) (*Parser, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+
+		return nil, err
+	}
+	defer file.Close()
+
+	agency_, err := strconv.Atoi(agency)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Parser{
+		file:      file,
+		maxBatch:  maxBatch,
+		bufReader: bufio.NewReader(file),
+		agency:    agency_,
+	}, nil
+}
+
+func (p *Parser) ReadBatch() (*BetBatch, error) {
+	batch := NewBetBatch()
+
+	for i := 0; i < p.maxBatch; i++ {
+		line, err := p.bufReader.ReadString('\n')
+
+		if err != nil {
+			if err == io.EOF {
+				if len(batch.Bets) > 0 {
+					return batch, nil
+				}
+				return nil, io.EOF // Devuelve EOF si no hay apuestas
+			}
+			return nil, err
+		}
+
+		bet, err := NewBetFromLine(line, p.agency)
+		if err != nil {
+			log.Warning(err)
+			continue
+		}
+
+		batch.AddBet(*bet)
+	}
+
+	return batch, nil
+
+}
+
+func (p *Parser) Close() error {
+	if p.file != nil {
+		return p.file.Close()
+	}
+	return nil
+}
